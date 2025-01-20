7eeff992587938add48d3d03a199e51bd30d7de3
Nguyen1976 <nguyen2202794@gmail.com>
2025-01-20 20:48:48 +0700

    theming in material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from "@mui/material/Button";
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div>
       <div>Nguyên đẹp trai</div>
+      <Typography variant="body2" color="text.secondary">Khà khà</Typography>
       <AccountBalanceIcon color="primary" />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
