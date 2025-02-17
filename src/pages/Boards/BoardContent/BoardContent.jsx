ab4d404e9806848e22b67f93b6f0d4be27f43c19
Nguyen1976 <nguyen2202794@gmail.com>
2025-02-17 22:11:46 +0700

    Done drag and drop column

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,21 +1,79 @@
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import ListColumns from "./ListColumns/ListColumns";
+
 import { mapOrder } from "~/utils/sorts";
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+
+//https://docs.dndkit.com/api-documentation/sensors Handle sensor
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, "_id");
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   // Require the mouse to move by 10 pixels before activating
+  //   activationConstraint: {
+  //     distance: 5,
+  //   },
+  // });
+  const mouseSensor = useSensor(MouseSensor, {
+    // Require the mouse to move by 10 pixels before activating
+    activationConstraint: {
+      distance: 10,
+    },
+  });
+  const touchSensor = useSensor(TouchSensor, {
+    // Require the mouse to move by 10 pixels before activating
+    activationConstraint: {
+      delay: 250,
+      tolerance: 500,
+    },
+  });
+  const sensors = useSensors(mouseSensor, touchSensor);
+
+  //Chúng ta đang muốn là map 1 mảng hiện tại sao cho nó được sắp xếp giống với mảng truyền vào bằng cahcs map qua 1 tham số ví dụ sắp xếp theo '_id
+  const [orderedColumns, setOrderedColumns] = useState([]);
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
+  }, [board]);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      //Lấy vị trí cũ từ thằng active
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id);
+      //Vị trí vừa thả ra từ thằng over
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id);
+
+      //Sử dụng arrayMove để tạo ra 1 mảng mới sau khi đã đổi vị trí của 2 column trong mảng
+      setOrderedColumns(arrayMove(orderedColumns, oldIndex, newIndex));
+
+      //call api cập nhật thứ tự column
+    }
+  };
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) =>
-          theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
-        width: "100%",
-        height: (theme) => theme.trello.boardContentHeight,
-        p: "10px 0",
-      }}
-    >
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
+          width: "100%",
+          height: (theme) => theme.trello.boardContentHeight,
+          p: "10px 0",
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   );
 }
 
