17d69f6c1be07bf9c39b5cd9c153e571582dda99
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-07 20:37:00 +0700

    Add placeholder cards for empty columns and update board state management

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -10,6 +10,8 @@ import {
   createNewColumnAPI,
   createNewCardAPI,
 } from "~/apis";
+import { generatePlaceholderCard } from "~/utils/formatters";
+import { isEmpty } from "lodash";
 
 function Board() {
   const [board, setBoard] = useState(null);
@@ -17,7 +19,14 @@ function Board() {
   useEffect(() => {
     const boardId = "67c868e67a889567f62a968d";
     //call-api
+    //Xử lý khi gen ra 1 column rỗng phải thêm 1 cái placehoderCard vào
     fetchBoardDetailsAPI(boardId).then((board) => {
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)];
+          column.cardOrderIds = [generatePlaceholderCard(column)._id];
+        }
+      });
       setBoard(board);
     });
   }, []);
@@ -32,22 +41,44 @@ function Board() {
       ...newColumnData,
       boardId: board._id,
     });
+
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)];
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id];
+
+    const newBoard = { ...board };
+    newBoard.columns.push(createdColumn);
+    newBoard.columnOrderIds.push(createdColumn._id);
+
+    setBoard(newBoard);
   };
 
   const createNewCard = async (newCardData) => {
-    const createdColumn = await createNewCardAPI({
+    const createdCard = await createNewCardAPI({
       ...newCardData,
       boardId: board._id,
     });
 
     //cập nhật state board
+    const newBoard = { ...board };
+    const columnToUpdate = newBoard.columns.find(
+      (column) => column._id === createdCard.columnId
+    );
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard);
+      columnToUpdate.cardOrderIds.push(createdCard);
+    }
+    setBoard(newBoard);
   };
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
       <AppBar />
       <BoardBar board={board} />
-      <BoardContent board={board} createNewColumn={createNewColumn} createNewCard={createNewCard} />
+      <BoardContent
+        board={board}
+        createNewColumn={createNewColumn}
+        createNewCard={createNewCard}
+      />
     </Container>
   );
 }
