a26ae905bca0c13a1818f83e93aeb4064183d1b1
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-10 10:19:24 +0700

    Add cross-env for environment variable management and update API_ROOT based on build mode

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  //Cho phép thằng vite dùng được process.env mặc định phải dùng import.meta.env
+  define: {
+    "process.env": process.env,
+  },
   plugins: [react()],
   server: {
     host: "0.0.0.0",
