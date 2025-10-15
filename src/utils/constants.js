a03928be336689e39dc6abb101864544ee77a140
Nguyen1976 <nguyen2202794@gmail.com>
2025-10-15 09:44:09 +0700

    Fix API_ROOT assignment to include port for development and production environments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // export const API_ROOT = 'http://localhost:8017';
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://3.94.162.118'
+  apiRoot = 'http://3.94.162.118:8017'
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://3.94.162.118'
+  apiRoot = 'http://3.94.162.118:8017'
 }
 
 export const API_ROOT = apiRoot
