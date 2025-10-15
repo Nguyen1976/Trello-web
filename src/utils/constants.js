eaff6b8d9c191a4b0d5dfad2b277284d122ad4ea
Nguyen1976 <nguyen2202794@gmail.com>
2025-10-15 09:34:49 +0700

    Update API_ROOT to use new server address for development and production environments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // export const API_ROOT = 'http://localhost:8017';
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://35.175.190.143:8017'
+  apiRoot = 'http://http://3.94.162.118'
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://35.175.190.143:8017'
+  apiRoot = 'http://http://3.94.162.118'
 }
 
 export const API_ROOT = apiRoot
