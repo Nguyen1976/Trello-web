99ea1042548ace79035481cbf1d54d71522f7a62
Nguyen1976 <nguyen2202794@gmail.com>
2025-10-16 20:37:11 +0700

    Update API_ROOT to use the production load balancer URL for development and production environments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // export const API_ROOT = 'http://localhost:8017';
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://localhost:8017'
+  apiRoot = 'http://trello-alb-1749900621.us-east-1.elb.amazonaws.com'
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://localhost:8017'
+  apiRoot = 'http://trello-alb-1749900621.us-east-1.elb.amazonaws.com'
 }
 
 export const API_ROOT = apiRoot
