423ab434fe3714489818b3ee9a58ee1e5c3e9251
Nguyen1976 <nguyen2202794@gmail.com>
2025-10-16 20:18:33 +0700

    Update API_ROOT to use localhost for development and production environments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // export const API_ROOT = 'http://localhost:8017';
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://50.19.56.60:8017'
+  apiRoot = 'http://localhost:8017'
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://50.19.56.60:8017'
+  apiRoot = 'http://localhost:8017'
 }
 
 export const API_ROOT = apiRoot
